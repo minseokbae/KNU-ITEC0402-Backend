90454ab22ec2ee0d875e4c548ad84852e3dea2b6
yjo78 <127858942+OwlEdu@users.noreply.github.com>
2024-10-06 13:54:24 +0900

    로그인 기능 1차 구현
    firebase.ts 추가해야합니다.
    로그인 상태 판별 기능은 아직입니다.

--- client/src/pages/LoginPage/index.tsx
+++ client/src/pages/LoginPage/index.tsx
@@ -7,11 +7,29 @@ import {
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { auth } from '../../firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
   const navigate = useNavigate();
 
   const [show, setShow] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setMessage('');
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      setMessage('Login successful');
+      navigate('/');
+    } catch (error) {
+      alert(`Error: ${(error as Error).message}`);
+    }
+  };
 
   const handleClick = () => {
     navigate("/signup");
@@ -20,13 +38,19 @@ const LoginPage = () => {
   return (
     <>
       <Heading>LoginPage</Heading>
-      <form>
-        <Input placeholder="Enter Email" />
+      <form onSubmit={handleSubmit}>
+        <Input
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} // 이메일 상태 업데이트
+        />
         <InputGroup size="md">
           <Input
             pr="4.5rem"
             type={show ? "text" : "password"}
             placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} // 비밀번호 상태 업데이트
           />
           <InputRightElement width="4.5rem">
             <Button h="1.75rem" size="sm" onClick={() => setShow(!show)}>
