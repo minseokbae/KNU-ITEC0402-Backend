8aee390e1fbdd60db3feb63d56f9041424ef8f9d
yjo78 <127858942+OwlEdu@users.noreply.github.com>
2024-10-06 23:32:24 +0900

    Login 상태 관리 context 완성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider, theme } from "@chakra-ui/react";
+import { AuthProvider } from './context/AuthContext';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -11,11 +12,13 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
-        <RouterProvider router={router} />
-      </ChakraProvider>
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={theme}>
+          <RouterProvider router={router} />
+        </ChakraProvider>
+      </QueryClientProvider>
+    </AuthProvider>
   );
 }
 
--- client/src/pages/HomePage/index.tsx
+++ client/src/pages/HomePage/index.tsx
@@ -1,16 +1,40 @@
+import { useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
+import { useAuth } from '../../context/AuthContext';
+import { signOut } from 'firebase/auth'; // Firebase signOut 함수 임포트
+import { auth } from '../../firebase'; // Firebase 인증 객체 임포트
 
 const HomePage = () => {
   const navigate = useNavigate();
+  const { user } = useAuth(); //Context에서 로그인 상태를 가져옴
+
+   // 로그아웃 함수
+   const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      console.log('User has been logged out');
+      navigate('/'); // 로그아웃 후 로그인 페이지로 이동
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
 
   const handleClick = () => {
-    navigate("/login");
+    if (user) {
+      handleLogout(); // 로그아웃 처리
+    } else {
+      navigate("/login"); // 로그인 페이지로 이동
+    }
   };
+
+   
 
   return (
     <div>
       HomePage
-      <button onClick={handleClick}>로그인하기</button>
+      <button onClick={handleClick}>
+        {user ? "로그아웃" : "로그인하기"}
+      </button>
     </div>
   );
 };
